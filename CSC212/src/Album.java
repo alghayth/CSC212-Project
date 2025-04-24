6c7a79864d83196f6c798fc416dbf5912ed4d8d0
munirah <munirah@localhost>
2025-04-24 20:08:35 +0300

    Initial Commit

---
+++ CSC212/src/Album.java
@@ -0,0 +1,124 @@
+
+public class Album {
+
+        private String name;
+        private String condition;
+        private PhotoManager manager;
+        private int NbComps;
+
+        // Constructor
+        public Album(String name, String condition, PhotoManager manager)
+        {
+            this.name = name;
+            this.condition = condition;
+            this.manager = manager;
+            NbComps =0 ;
+        }
+
+        // Return the name of the album
+        public String getName()
+        {
+            return name;
+        }
+        
+        // Return the condition associated with the album
+        public String getCondition()
+        {
+            return condition;
+        }
+
+        // Return the manager
+        public PhotoManager getManager()
+        {
+            return manager;
+        }
+        
+        // Return all photos that satisfy the album condition
+        public LinkedList<Photo> getPhotos()
+        {
+                LinkedList<Photo> Myphotos = new LinkedList<Photo>();
+                {
+                    LinkedList<Photo> photos1 = manager.getPhotos();
+                    if (! photos1.empty())
+                    {
+                        photos1.findFirst();
+                        while (! photos1.last())
+                        {
+                        	 Myphotos.insert(new Photo(photos1.retrieve().getPath(), photos1.retrieve().getTags()));
+                            photos1.findNext();
+                        }
+                        Myphotos.insert(new Photo(photos1.retrieve().getPath(), photos1.retrieve().getTags()));
+                    }
+                }
+                NbComps =0 ;
+                
+                if (this.condition.compareTo("") != 0)
+                {
+                    String [] Array = condition.split(" AND ");
+                    
+                    Myphotos.findFirst();
+                    while ( !  Myphotos.last())
+                    {
+                        Photo photo = Myphotos.retrieve();
+                        //System.out.println("test " + photo.getPath());
+                        if ( ! allAvilable (photo.allTags , Array ))
+                        	 Myphotos.remove();
+                        else
+                        	 Myphotos.findNext();
+                    }
+                    Photo photo11 =  Myphotos.retrieve();
+                    //System.out.println("testlast " + photo11.getPath());
+                    if ( ! allAvilable (photo11.allTags , Array ))
+                    	 Myphotos.remove();
+                    else
+                    	 Myphotos.findNext();
+                }
+                return  Myphotos;
+        }
+       
+        // Return the number of tag comparisons used to find all photos of the album
+        public int getNbComps()
+        {
+            return NbComps;
+        }
+
+        private boolean allAvilable ( LinkedList<String> AllTags , String [] Array )
+        {
+            boolean continue1 = true;
+            if (AllTags.empty())
+                continue1 = false;
+            else
+            {
+                for ( int i = 0 ; i < Array.length && continue1 ; i++)
+                {
+                    boolean found_in_tags = false;
+
+                    AllTags.findFirst();
+
+                    while (!AllTags.last())
+                    {
+                        this.NbComps ++ ;    
+                        //System.out.println(AllTags.retrieve() + " " + Array[i]);
+                        if (AllTags.retrieve().compareToIgnoreCase(Array[i]) == 0)
+                        {
+                            found_in_tags = true;
+                            break;
+                        }
+                        AllTags.findNext();
+                    }
+                    if (! found_in_tags )
+                    {
+                        this.NbComps ++ ;
+                        //System.out.println(AllTags.retrieve() + " " + Array[i]);
+                        if (AllTags.retrieve().compareToIgnoreCase(Array[i]) == 0)
+                            found_in_tags = true;
+                    }
+                    if ( ! found_in_tags )
+                        continue1 = false;
+                }
+            }
+            return continue1;
+        }
+       
+}
+
---
+++ CSC212/src/PhotoManager.java
@@ -0,0 +1,81 @@
+
+public class PhotoManager {
+	
+	    
+	     LinkedList<Photo> photos;
+
+	     // Constructor
+	    public PhotoManager()
+	    {
+	            photos = new LinkedList<Photo>();
+	    }
+	    
+	    // Add a photo
+	    public void addPhoto(Photo p)
+	    {
+	           if (! IsPhototAvailable(p.getPath(), photos) )     
+	               photos.insert(p);
+	    }
+	    
+	    private boolean IsPhototAvailable(String p, LinkedList<Photo> List)
+	    {
+	        if (List.empty()) 
+	           return false;
+
+	        List.findFirst();
+	        while ( !List.last())
+	        {
+	           if (List.retrieve().getPath().compareToIgnoreCase(p)== 0)
+	               return true;
+
+	           List.findNext();
+	        }
+
+	        if (List.retrieve().getPath().compareToIgnoreCase(p)== 0)
+	           return true;
+
+	        return false;
+	    }
+
+	    // Delete a photo
+	    public void deletePhoto(String path)
+	    {
+	        if (! this.IsPhototAvailable(path, photos))
+	            return;
+	    
+	        if (! photos.empty())
+	         {
+	            boolean found = false;
+	        
+	            photos.findFirst();
+	            while (!found && !photos.last() )
+	            {
+	                Photo p = photos.retrieve();
+	                if (p.getPath().compareToIgnoreCase(path) == 0)
+	                {
+	                    found = true; 
+	                    photos.remove();
+	                }
+	                photos.findNext();
+	            }
+	            
+	            if (!found )
+	            {
+	                Photo p = photos.retrieve();
+	                if (p.getPath().compareToIgnoreCase(path) == 0)       
+	                {
+	                    found = true;
+	                    photos.remove();
+	                }
+	            }
+	        }
+	    }
+
+	    // Return all managed photos
+	    public LinkedList<Photo>  getPhotos()
+	    {
+	        return photos;
+	    }
+	}
+	
+
